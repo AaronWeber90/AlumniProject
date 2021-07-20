2f7d5e950e69e42d5212e57950d7c787fa30c6d8
BarbaraGaluppo <barbaragaluppo@gmail.com>
2021-07-20 19:45:02 +0200

    enters students profiles

---
+++ cf-alumni/src/app/students-profiles.ts
@@ -0,0 +1,38 @@
+export let students = [
+    {
+        name: 'Maria Pichler',
+        graduation: "Graduated in 2019",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit provident eum autem, atque quo repellendus perferendis ipsum reiciendis sed voluptatum.",
+        image: "https://cdn.pixabay.com/photo/2013/04/02/13/06/beach-99388__340.jpg"
+    },
+    {
+        name: 'Julian Wagner',
+        graduation: "Graduated in 2020.",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit provident eum autem, atque quo repellendus perferendis ipsum reiciendis sed voluptatum.",
+        image: "https://cdn.pixabay.com/photo/2013/04/02/13/06/beach-99388__340.jpg"
+    },
+    {
+        name: 'Anna Moser',
+        graduation: "Graduated in 2021.",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit provident eum autem, atque quo repellendus perferendis ipsum reiciendis sed voluptatum.",
+        image: "https://cdn.pixabay.com/photo/2013/04/02/13/06/beach-99388__340.jpg"
+    },
+    {
+        name: 'Dario Weber',
+        graduation: "Graduated in 2018",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit provident eum autem, atque quo repellendus perferendis ipsum reiciendis sed voluptatum.",
+        image: "https://cdn.pixabay.com/photo/2013/04/02/13/06/beach-99388__340.jpg"
+    },
+    {
+        name: 'Ella Winkler',
+        graduation: "Graduated in 2020",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit provident eum autem, atque quo repellendus perferendis ipsum reiciendis sed voluptatum.",
+        image: "https://cdn.pixabay.com/photo/2013/04/02/13/06/beach-99388__340.jpg"
+    },
+    {
+        name: 'Otto Hoffmann ',
+        graduation: "JGraduated in 2018",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit provident eum autem, atque quo repellendus perferendis ipsum reiciendis sed voluptatum.",
+        image: "https://cdn.pixabay.com/photo/2013/04/02/13/06/beach-99388__340.jpg"
+    },
+];
