c12b822d3b58b88322977850dfd6aa0c2326615e
philipm92 <philip.mahlberg@hotmail.com>
2021-07-21 11:32:09 +0200

    alumni stuff uin progress

--- cf-alumni/src/app/alumni-dir/alumni-dir.component.ts
+++ cf-alumni/src/app/alumni-dir/alumni-dir.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import StudentstData from 'src/assets/json/alumni.json';
+import { SortDesc, GetFirstElem } from '../usefulfunctions';
 
 @Component({
   selector: 'app-alumni-dir',
@@ -8,12 +9,14 @@ import StudentstData from 'src/assets/json/alumni.json';
 })
 export class AlumniDirComponent implements OnInit {
   students_list: any = StudentstData;
+  first_element: any = GetFirstElem(this.students_list, true);
+  constructor() { }
 
-  constructor() { 
-    // dont know what to do here
+  GetGraduationYear(student_id: number): number {
+    let date = new Date(this.students_list[student_id].date);
+    return date.getFullYear();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
